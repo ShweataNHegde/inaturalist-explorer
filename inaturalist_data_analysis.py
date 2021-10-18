1e536c9ad1ff374218575697764f78ee2abf877c
Shweata N Hegde <shweata.hegde@gmail.com>
2021-10-18 12:48:50 +0530

    cleaned code

--- inaturalist_data_analysis.py
+++ inaturalist_data_analysis.py
@@ -12,24 +12,19 @@ def grab_relevant_columns_make_new_df(df, required_columns = ['latitude', 'longi
     return filtered_df
 
 def make_plot(filtered_df):
-        
-    #empty map
-    locations = filtered_df[['latitude', 'longitude']]
-    # droping NaNs
-    locations=locations.dropna()
+    """Map code as per: https://github.com/petermr/dictionary/blob/main/interactive_map/geotext_geopy_map_oil186.ipynb
 
+    Args:
+        filtered_df (dataframe): a new dataframe with information relevant to plotting on worldmap
+    """
+    locations = filtered_df[['latitude', 'longitude']]
+    locations=locations.dropna()
     locationlist = locations.values.tolist()
     world_map= folium.Map(tiles="cartodbpositron")
-    marker_cluster = MarkerCluster().add_to(world_map)
-
     df_with_info_other_than_lat_long=filtered_df.drop(['latitude','longitude'], axis=1)
-
-    #for each coordinate, create circlemarker 
     for i in range(0, len(locationlist)):
             df1=df_with_info_other_than_lat_long.iloc[i].to_frame()
             folium.Marker(locationlist[i],popup= df1.to_html(),icon=folium.Icon(color='darkblue',icon= 'default',prefix='fa')).add_to(world_map)
-            #folium.CircleMarker(locationlist[i], radius=radius, popup= df1.to_html(), fill =True, ).add_to(marker_cluster)
-
     world_map.save('dragon_fly_india.html')
 
 def main():
